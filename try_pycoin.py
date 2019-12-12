822f44cec2eecc83329ff2da5cf26690cdcdff4a
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-12-12 10:43:49 +0000

    try pycoin update

--- try_pycoin.py
+++ try_pycoin.py
@@ -1,36 +1,61 @@
 import random
+import requests
+import inspect
+import pprint
 from pycoin.symbols.btc import network
+
+# ---
+def pp(string):
+  p = pprint.PrettyPrinter(indent=2)
+  p.pprint(string)
 
 
 rand = random.getrandbits(256)
 
 priv_key = network.keys.private(secret_exponent=rand)
-# print("private key:")
-# print(priv_key.wif())
+address = priv_key.address()
+print("private key:")
+print(priv_key.wif())
+
+print("address:")
+print(address)
+
+
+tx_id = "11e0a8a0ced85b8fd854527d8eb0e675bbba411ea5e04df75331908f9c4363a2"
+URL = f"https://blockchain.info/rawtx/{tx_id}"
+
+req = requests.get(url=URL)
+data = req.json()
+
+pp(req)
+pp(data)
+
+exit
+
 
 # bip32 - https://github.com/richardkiss/pycoin#bip32
 
-print("address:")
-print(priv_key.address())
-
-address = priv_key.address()
 
 
-# TODO: read transaction from blockcypher
-tx_hex = "...."
 
-# get the spendable from the prior transaction
-tx = network.tx.from_hex(tx_hex)
-tx_out_index = 0
-spendable = tx.tx_outs_as_spendable()[tx_out_index]
 
-# make sure the address is valid
-assert network.parse.address(address) is not None
 
-# create the unsigned transaction
-tx = network.tx_utils.create_tx([spendable], [payable])
-
-print("here is the transaction: %s" % tx.as_hex(include_unspents=True))
+#
+# # TODO: read transaction from blockcypher
+# tx_hex = "...."
+#
+# # get the spendable from the prior transaction
+# tx = network.tx.from_hex(tx_hex)
+# tx_out_index = 0
+# spendable = tx.tx_outs_as_spendable()[tx_out_index]
+#
+# # make sure the address is valid
+# assert network.parse.address(address) is not None
+#
+# # create the unsigned transaction
+# tx = network.tx_utils.create_tx([spendable], [payable])
+#
+# print("here is the transaction: %s" % tx.as_hex(include_unspents=True))
 
 
 # sign TX
