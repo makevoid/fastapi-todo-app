905c3cd51401fa4088af2528a2065b5ae3a34998
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-12-12 16:48:43 +0000

    complete?

--- try_pycoin.py
+++ try_pycoin.py
@@ -3,6 +3,7 @@ import requests
 import inspect
 import pprint
 from pycoin.symbols.btc import network
+from pycoin.encoding.hexbytes import h2b
 
 # ---
 def pp(string):
@@ -31,6 +32,10 @@ print("rawtx bcinfo:")
 pp(req)
 pp(data)
 
+
+output_index = 0
+output_script = data["out"][output_index]["script"]
+
 url_rawtx_insight = f"https://insight.bitpay.com/api/rawtx/{tx_id}"
 
 req = requests.get(url=url_rawtx_insight)
@@ -41,8 +46,7 @@ pp(req)
 tx_raw = data["rawtx"]
 
 tx_prev = network.tx.from_hex(tx_raw)
-tx_prev_out_index = 0
-spendable_output = tx_prev.tx_outs_as_spendable()[tx_prev_out_index]
+spendable_output = tx_prev.tx_outs_as_spendable()[output_index]
 
 print("prev tx:")
 pp(tx_prev)
@@ -54,28 +58,18 @@ assert network.parse.address(address) is not None
 
 tx = network.tx_utils.create_tx([spendable_output], [address])
 
+print("tx:")
+pp(tx)
 
-exit
+output_scripts = [h2b(output_script)]
+p2sh_lookup = network.tx.solve.build_p2sh_lookup(output_scripts)
 
+# sign the transaction with the given WIF
+network.tx_utils.sign_tx(tx, wifs=[priv_key.wif()], p2sh_lookup=p2sh_lookup)
 
-# bip32 - https://github.com/richardkiss/pycoin#bip32
+bad_solution_count = tx.bad_solution_count()
+print("tx %s now has %d bad solution(s)" % (tx.id(), bad_solution_count))
 
-
-
-
-
-
-
-# # get the spendable from the prior transaction
-
-#
-# # make sure the address is valid
-#
-# # create the unsigned transaction
-# tx = network.tx_utils.create_tx([spendable], [payable])
-#
-# print("here is the transaction: %s" % tx.as_hex(include_unspents=True))
-
-
-# sign TX
-# https://github.com/richardkiss/pycoin/blob/master/recipes/multisig/4_sign_tx.py
+include_unspents = (bad_solution_count > 0)
+print("TX - serialized:")
+print(tx.as_hex(include_unspents=include_unspents))
