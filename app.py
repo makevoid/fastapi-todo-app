dcaa012a8aa66d65bdc7165ccabde7ab4540e816
makevoid <makevoid@gmail.com>
2020-08-28 00:30:07 +0100

    update flask app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 import haml
 import mako.template
 import mako.lookup
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -11,13 +12,14 @@ lookup = mako.lookup.TemplateLookup(["views"],
 
 @app.route('/')
 def hello():
-    foo = request.args.get('foo') /?foo=bar
+    foo = request.args.get('foo')
     test = "1234"
     template = lookup.get_template('home.haml')
     return template.render(test=test, foo=foo)
 
-@app.route('/foo', methods=["POST"])
-def foo():
+# @app.route('/foo', methods=["POST"])
+# def foo():
 
 if __name__ == '__main__':
     app.run()
+    # serve(app, host='0.0.0.0', port=3000)
