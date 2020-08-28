f7684ef6487da879687fd32ac41b54956c640b27
makevoid <makevoid@gmail.com>
2020-08-28 04:18:25 +0100

    cache template lookup

--- app.py
+++ app.py
@@ -10,14 +10,29 @@ r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 lookup = mako.lookup.TemplateLookup(["views"], preprocessor=haml.preprocessor)
 
+TEMPLATES = {}
+
+def lookup_template(template_name):
+    template = TEMPLATES.get(template_name)
+    if template: return template
+    template = lookup.get_template(f'{template_name}.haml')
+    TEMPLATES[template_name] = template
+    return template
+
+
+def render(template_name, **args):
+    template = lookup_template(template_name)
+    return template.render(**args)
+
 @app.route('/')
 def hello():
     r.set('foo', 'bar')
     # foo = request.args.get('foo')
     test = "1234"
-    template = lookup.get_template('home.haml')
     foo = r.get('foo')
-    return template.render(test=test, foo=foo)
+    # template = lookup.get_template('home.haml')
+    # return template.render(test=test, foo=foo)
+    return render("home", test=test, foo=foo)
 
 # @app.route('/foo', methods=["POST"])
 # def foo():
