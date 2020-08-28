69100a9ca2c470616ef24772aa0058166643f9cd
makevoid <makevoid@gmail.com>
2020-08-28 14:25:04 +0100

    draft todos

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request
 from waitress import serve
 from env import R, flask_options
@@ -6,7 +7,7 @@ from lib import render
 app = Flask(__name__, **flask_options)
 
 @app.route('/')
-def hello():
+def root():
     R.set('foo', 'bar')
     # foo = request.args.get('foo')
     test = "1234"
@@ -17,8 +18,61 @@ def hello():
 def hello2():
     return render("page2")
 
-# @app.route('/foo', methods=["POST"])
-# def foo():
+class Todo(object):
+    """Todo items model"""
+
+    def __init__(self, text, checked=false):
+        super(Todo, self).__init__()
+        self.text = text
+        self.checked = checked
+        todos_count = R.incr('todos_count')
+        self.id = todos_count
+        save()
+
+    def check():
+        self.checked = !self.checked
+        save()
+        return self
+
+    def update_text(text):
+        self.text = text
+        save()
+        return self
+
+    def save():
+        todo = json.dumps(self.__dict__)
+        R.set(f'todos:${id}', todo)
+        return self
+
+    @classmethod
+    def get(todo_id)
+        todo = R.get(f'todos:${todo_id}')
+        todo = json.loads(todo)
+        todo = new Todo
+        return todo
+
+    @classmethod
+    def all()
+        todos_count = R.get('todos_count')
+        todos_count = int(todos_count)
+        todos = []
+        for i in range(todos_count):
+            todo = self.get(i)
+            todos.push(todo)
+        return todos
+
+@app.route('/todos', methods=["POST"])
+def todo_create():
+    todo = Todo(text)
+    print(f'Todo created: {todo.__dict__}')
+    redirect(url_for('root'))
+
+@app.route('/todos/<int:todo_id>', methods=["POST"])
+def todo_update(todo_id):
+    todo = Todo(text)
+    print(f'Todo created: {todo.__dict__}')
+    redirect(url_for('root'))
+
 
 if __name__ == '__main__':
     app.run()
